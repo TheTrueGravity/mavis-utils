ce2e46af3d8c183f8aeb8a3a6ac2534cfa109dee
Charlie Armstrong <charliearmstrong180@gmail.com>
2020-12-29 16:27:47 +0000

    main

--- src/speak.js
+++ src/speak.js
@@ -1,7 +1,16 @@
-const say = require('say');
+const gtts = require('node-gtts')('en')
+const fs = require('fs');
+const { spawn } = require("child_process");
 
 module.exports = {
     run: function(text_to_speak) {
-        say.speak(text_to_speak)
+        gtts.save('./src/audio.wav', text_to_speak, () => {
+            var ffplay = spawn('ffplay', ['src/audio.wav', '-nodisp', '-autoexit'])
+            ffplay.on("close", code => {
+                fs.unlinkSync('./src/audio.wav')
+            });
+        })
     }
 }
+
+module.exports.run("Hello, I'm mavis, your personal assistant!")
